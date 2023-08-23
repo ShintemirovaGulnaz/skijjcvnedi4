bc3ce99a8b1227ef766dfb6191550dce5ba21739
Shintemirova Gulnaz <shintemirova.gulnaz@syk.fi>
2023-08-23 15:18:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int A=14;
+        int B= 14;
+        boolean x = (A==B);
+        if (x)
+            System.out.println("output is=" + (A*A + B*B));
+        else
+            System.out.println("output is=" + (A+B));
+    }
+}
